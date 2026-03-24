334907b25faa52f3dbf5d946eb483ab990ba5158
Mohammad Sitthik <sitthik180222004@gmail.com>
2026-03-24 23:08:07 +0530

    fixed login issue

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors"); // 🔥 ADD THIS
 
 const app = express();
 app.use(express.json());
+app.use(cors()); // 🔥 MUST ADD
+const authRoutes = require("./routes/authRoutes");
+
+app.use("/api/auth", authRoutes);
 
 // MongoDB connect
 mongoose.connect("mongodb://sitthik180222004_db_user:sitthik123@ac-fmmqaqe-shard-00-00.a2aoav1.mongodb.net:27017,ac-fmmqaqe-shard-00-01.a2aoav1.mongodb.net:27017,ac-fmmqaqe-shard-00-02.a2aoav1.mongodb.net:27017/?ssl=true&replicaSet=atlas-mlvl93-shard-0&authSource=admin&appName=Cluster0")
